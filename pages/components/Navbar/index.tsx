4f003c903d36758428bcb920ad9d4d2ce1c5928f
Mayank Kumar Sharma <mayankece155@gmail.com>
2023-12-18 13:07:01 +0530

    active link on navbar

--- pages/components/Navbar/index.tsx
+++ pages/components/Navbar/index.tsx
@@ -1,8 +1,11 @@
 import Image from 'next/image'
 import styles from '@/app/page.module.css'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export default function Navbar() {
+  const pathname = usePathname()
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.navContainer}>
@@ -16,13 +19,21 @@ export default function Navbar() {
         />
         <div className={styles.description}>
           <Link href="/" className={styles.cardTextDesc}>
-            <p className={styles.navTopLink}>Home</p>
+            {pathname === '/' ? (
+              <p className={styles.navTopLink}>Home</p>
+            ) : (
+              'Home'
+            )}
           </Link>
           <Link
             className={styles.cardTextDesc}
             href={{ pathname: '/products', query: { page: 1 } }}
           >
-            Products
+            {pathname === '/products' ? (
+              <p className={styles.navTopLink}>Products</p>
+            ) : (
+              'Products'
+            )}
           </Link>
         </div>
       </div>
